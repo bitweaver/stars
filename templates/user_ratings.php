e5ce1e7f20d91f3219c2dbcc653ab98ff6eccabc
Andy Stanford <phoenixandy@users.sourceforge.net>
2007-02-14 14:59:27 +0000

    Fix divide by zero error if user has added no ratings

--- templates/user_ratings.php
+++ templates/user_ratings.php
@@ -10,7 +10,13 @@ $sum = 0;
 foreach( $userRatings as $rating ) {
 	$sum += $rating['user_rating'];
 }
-$average = round( $sum / count( $userRatings ));
+
+if (count( $userRatings ) > 0 ) {
+	$average = round( $sum / count( $userRatings ));
+} else {
+	$average = 0;
+}
+
 $stars = $gBitSystem->getConfig( 'stars_used_in_display', 5 );
 $pixels = $stars *  $gBitSystem->getConfig( 'stars_icon_width', 22 );
 $average_pixels = $average * $pixels / 100;
