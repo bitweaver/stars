56b6a4a88d567dd5c6a7baedde014afc794e1f76
wjames5 <will@tekimaki.com>
2009-10-01 13:45:48 +0000

    fix LGPL license ref from non-existant license.txt file to lgpl url

--- admin/admin_stars_inc.php
+++ admin/admin_stars_inc.php
@@ -1,8 +1,8 @@
 <?php
-// $Header: /cvsroot/bitweaver/_bit_stars/admin/admin_stars_inc.php,v 1.3 2007/06/22 20:24:21 nickpalmer Exp $
+// $Header: /cvsroot/bitweaver/_bit_stars/admin/admin_stars_inc.php,v 1.4 2009/10/01 13:45:48 wjames5 Exp $
 // Copyright (c) 2005 bitweaver Stars
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
-// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See http://www.gnu.org/copyleft/lesser.html for details.
 
 require_once( STARS_PKG_PATH.'LibertyStars.php' );
 $gBitSmarty->assign_by_ref( 'feedback', $feedback = array() );
