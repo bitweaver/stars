39c68b6b36addfbbe3f618e572ab58b4730ba50d
wjames5 <will@tekimaki.com>
2009-10-01 14:17:05 +0000

    remove ref to non-existant copyright.txt file

--- admin/admin_stars_inc.php
+++ admin/admin_stars_inc.php
@@ -1,7 +1,7 @@
 <?php
-// $Header: /cvsroot/bitweaver/_bit_stars/admin/admin_stars_inc.php,v 1.4 2009/10/01 13:45:48 wjames5 Exp $
+// $Header: /cvsroot/bitweaver/_bit_stars/admin/admin_stars_inc.php,v 1.5 2009/10/01 14:17:05 wjames5 Exp $
 // Copyright (c) 2005 bitweaver Stars
-// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// All Rights Reserved. See below for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See http://www.gnu.org/copyleft/lesser.html for details.
 
 require_once( STARS_PKG_PATH.'LibertyStars.php' );
